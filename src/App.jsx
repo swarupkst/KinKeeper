ce127fd5c90ef30f8bddd2245029c24d0bcd73f8
Swarup <swarupkst@gmail.com>
2026-04-13 19:12:07 +0600

    Setup done and initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './navbar/Navbar'
+import Body from './body/Body'
+import Footer from './footer/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Body />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/body/Body.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <div>
+      <h2>Main Body</h2>
+    </div>
+  )
+}
+
+export default Body
